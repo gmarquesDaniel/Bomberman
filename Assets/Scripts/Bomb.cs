63a1cd516eb552fed67a37959c47ad11e32dbe16
Daniel Godoy Marques <dan.gmarques@hotmail.com>
2019-11-05 19:45:10 -0300

    Bomb destroying boxes still bugged, but now it creates an explosion on box position

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -8,6 +8,7 @@ public class Bomb : MonoBehaviour {
     public int bombRange = 1;
     public float timer = 3;
     public LayerMask blockingLayer;
+    public LayerMask boxLayer;
     public GameObject explosion;
     private Player owner;
 
@@ -21,18 +22,29 @@ public class Bomb : MonoBehaviour {
         Vector3 bombPosition = transform.position;
         // int x = (int) bombPosition.x;
         // int y = (int) bombPosition.y;    
-        RaycastHit2D hit;
+        RaycastHit2D hit, boxHit;
         Vector3[] explosionDirections = new Vector3[4] { new Vector3(-1, 0, 0f), new Vector3(1, 0, 0f), new Vector3(0, 1, 0f), new Vector3(0, -1, 0f) };
+        // blockingLayer = LayerMask.NameToLayer("BlockingLayer");
 
         Instantiate(explosion, bombPosition, Quaternion.identity);
         for (int i = 1; i <= bombRange; i++) {
             for (int j = 0; j < 4; j++) {
                 boxCollider.enabled = false;
                 hit = Physics2D.Linecast((explosionDirections[j] * (i - 1)) + bombPosition, (explosionDirections[j] * i) + bombPosition, blockingLayer);
+                boxHit = Physics2D.Linecast((explosionDirections[j] * (i - 1)) + bombPosition, (explosionDirections[j] * i) + bombPosition, boxLayer);
                 boxCollider.enabled = true;
 
                 if (hit.transform == null) {
                     Instantiate(explosion, (explosionDirections[j] * i) + bombPosition, Quaternion.identity);
+                }
+
+                if (boxHit.collider) {
+                    print(boxHit.collider.transform.position);
+                    Box hitReciver = boxHit.collider.gameObject.GetComponent<Box>();
+                    if (hitReciver != null) {
+                        print("colidiu com a box");
+                        hitReciver.Break();
+                    }
                 }
             }
 
--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -7,14 +7,19 @@ public class Explosion : MonoBehaviour {
     public float timer = 0.7f;
 
     // Start is called before the first frame update
+    void Awake() {
+        print("born");
+    }
+
     void Start() {
 
     }
 
     // explosion collided with something
-    void OnTriggerEnter2D(Collider2D col) {
+    void OnTriggerStay2D(Collider2D col) {
         //hit box
-        if (col.gameObject.layer == LayerMask.NameToLayer("BoxLayer")) {
+        print("colidiu ");
+        if (col.gameObject.layer == LayerMask.NameToLayer("BoxLayer ")) {
             Box box = col.GetComponent<Box>();
             box.Break();
             // hit player
@@ -22,7 +27,7 @@ public class Explosion : MonoBehaviour {
             Player player = col.GetComponent<Player>();
             player.LoseHealth(1);
             // hit item
-        } else if (col.gameObject.layer == LayerMask.NameToLayer("ItemLayer")) {
+        } else if (col.gameObject.layer == LayerMask.NameToLayer("ItemLayer ")) {
             // Item item = col.GetComponent<Item>();
             // Destroy(item);
         }
